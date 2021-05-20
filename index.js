635dde026372a477883d404f18e2fe7bd8c2ef48
Oksana Martynenko <oks.martynenko@gmail.com>
2021-05-21 02:17:58 +0300

    Добавляет js для кнопок

---
+++ index.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-action=start]');
+const stopButton = document.querySelector('button[data-action=stop]');
+let intervalId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function setBodyBackgColor() {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(1, colors.length)];
+}; 
+
+startButton.addEventListener('click', onStartBtnClick);
+stopButton.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startButton.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    setBodyBackgColor() 
+  }, 1000);
+};
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  startButton.removeAttribute('disabled');
+};
