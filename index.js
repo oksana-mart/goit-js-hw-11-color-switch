15d0af71e0ffe8a16b04458471a341e46bc5b51a
Oksana Martynenko <oks.martynenko@gmail.com>
2021-05-30 22:14:28 +0300

    Добавляет проверку на "старт"

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const colors = [
   '#795548',
 ];
 
-const body = document.querySelector('body');
+const body = document.body;
 const startButton = document.querySelector('button[data-action=start]');
 const stopButton = document.querySelector('button[data-action=stop]');
 let intervalId = null;
@@ -24,13 +24,16 @@ startButton.addEventListener('click', onStartBtnClick);
 stopButton.addEventListener('click', onStopBtnClick);
 
 function onStartBtnClick() {
+  if (intervalId) {
+    return;   
+  };
   startButton.setAttribute('disabled', true);
   intervalId = setInterval(() => {
     setBodyBackgColor() 
   }, 1000);
 };
-
 function onStopBtnClick() {
   clearInterval(intervalId);
+  intervalId = null;
   startButton.removeAttribute('disabled');
 };
